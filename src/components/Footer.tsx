4ea2a60c9fc7fe1b002e0a7210c442634612582a
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2023-05-15 21:35:50 -0600

    se avanzo en el proyecto logrando el primer fetch

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-red-600 text-white content-center rounded-2xl flex ">
+      <ul>
+        <li>fb</li>
+        <li>insta</li>
+      </ul>
+    </footer>
+  );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar() {
+    
+  return (
+    <nav className=" flex bg-red-600 text-white justify-between">
+      <h1>NAVBAR</h1>
+    </nav>
+  );
+}
